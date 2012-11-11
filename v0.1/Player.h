7d7f8500e6a9b14d2c34f0feb34a9aeaa1231f09
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2012-11-11 12:00:56 +0000

    r7: dxライブラリのベースです。　どうぞ使ってください

---
+++ v0.1/Player.h
@@ -0,0 +1,19 @@
+#ifndef DEF_PLAYER_H //“ñdinclude–h~
+
+#define DEF_PLAYER_H
+
+struct Player;
+
+// ‰Šú‰»‚ğ‚·‚é
+Player *Player_Initialize( int y, int img );
+
+// “®‚«‚ğŒvZ‚·‚é
+void Player_Update( Player *Player );
+
+// •`‰æ‚·‚é
+void Player_Draw( Player *Player );
+
+// I—¹ˆ—‚ğ‚·‚é
+void Player_Finalize( Player *Player );
+
+#endif
---
+++ v0.1/main.cpp
@@ -0,0 +1,21 @@
+#include "DxLib.h"
+#include "Game.h"
+
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+{
+    ChangeWindowMode(TRUE),DxLib_Init(),SetDrawScreen( DX_SCREEN_BACK );
+
+	Game *game = Game_Initialize();
+
+    while( ScreenFlip()==0 && ProcessMessage()==0 && ClearDrawScreen()==0 )
+	{
+		Game_Update(game);
+		Game_Draw(game );
+
+    }
+
+	Game_Finalize(game);
+
+    DxLib_End();
+    return 0;
+}
