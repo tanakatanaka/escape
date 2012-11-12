d8504c262e3923b17bb0e1c6926d2171904ed8ae
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2012-11-12 15:20:00 +0000

    r14

--- Room.cpp
+++ Room.cpp
@@ -5,6 +5,8 @@
 struct Room
 {
     int model;
+	VECTOR cam;
+	double rotY;
 }; 
 
 // ‰Šú‰»‚ð‚·‚é
@@ -12,23 +14,35 @@ Room *Room_Initialize()
 {
 	Room *self;
 	self = (Room *)malloc(sizeof(Room));
-    self->model = MV1LoadModel("room.mqo") ;    //‰æ‘œƒnƒ“ƒhƒ‹‚ÌŠi”[
-	MV1SetPosition(self->model, VGet( 320.0f, -300.0f, 600.0f ) ) ;
+    self->model = MV1LoadModel("meta/room.mqo") ;    //‰æ‘œƒnƒ“ƒhƒ‹‚ÌŠi”[
+	MV1SetScale(self->model,VGet(10,10,10));
+	self->cam = VGet(0.0f,500.0f,-500.0f);
+	self->rotY = 0.0f;
 	return self;
 }
 
 // “®‚«‚ðŒvŽZ‚·‚é
 void Room_Update( Room *self )
 {
-    if( Keyboard_Get( KEY_INPUT_UP   ) > 0 )
-	{//ã‚ª‰Ÿ‚³‚ê‚Ä‚¢‚½‚ç
-       
-    }
-    if( Keyboard_Get( KEY_INPUT_DOWN ) > 0 )
-	{//‰º‚ª‰Ÿ‚³‚ê‚Ä‚¢‚½‚ç
+	//
+
+	if( 1==CheckHitKey(KEY_INPUT_LEFT) ) self->cam.x-=5;
+        if( 1==CheckHitKey(KEY_INPUT_RIGHT) ) self->cam.x+=5;
+        if( 1==CheckHitKey(KEY_INPUT_LSHIFT) || 1==CheckHitKey(KEY_INPUT_RSHIFT) ) {
+            if( 1==CheckHitKey(KEY_INPUT_UP) ) self->cam.z+=5;
+            if( 1==CheckHitKey(KEY_INPUT_DOWN) ) self->cam.z-=5;
+        } else {
+            if( 1==CheckHitKey(KEY_INPUT_UP) ) self->cam.y+=5;
+            if( 1==CheckHitKey(KEY_INPUT_DOWN) ) self->cam.y-=5;
+        }
+        SetCameraPositionAndTarget_UpVecY(self->cam,VGet(0.0f,0.0f,0.0f));
         
-    }
-}
+
+		MV1SetPosition(self->model, VGet( 200.0f, 0.0f, 300.0f ) );
+        MV1SetRotationXYZ( self->model, VGet( 0.0f, self->rotY, 0.0f ) );
+
+		self->rotY += PHI / 180.0f;
+}		
 
 // •`‰æ‚·‚é
 void Room_Draw( Room *self)
