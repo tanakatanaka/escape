8889c7b2ec92183dbe84972def2deac5d05768f4
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2012-11-29 16:57:45 +0000

    r37

---
+++ Pad.cpp
@@ -0,0 +1,28 @@
+#include "DxLib.h"
+#include "Pad.h"
+
+struct Pad
+{
+   
+}; 
+
+// ‰Šú‰»‚ğ‚·‚é
+Pad *Pad_Initialize( )
+{
+	Pad *self;
+	self = (Pad *)malloc(sizeof(Pad));
+	return self;
+}
+
+// “®‚«‚ğŒvZ‚·‚é
+void Pad_Update( Pad *self )
+{
+    
+}
+
+
+// I—¹ˆ—‚ğ‚·‚é
+void Pad_Finalize( Pad *self )
+{
+    
+}
---
+++ Pad.h
@@ -0,0 +1,17 @@
+#ifndef DEF_Pad_H //“ñdinclude–h~
+
+#define DEF_Pad_H
+
+struct Pad;
+
+// ‰Šú‰»‚ğ‚·‚é
+Pad *Pad_Initialize( );
+
+// “®‚«‚ğŒvZ‚·‚é
+void Pad_Update( Pad *Pad );
+
+
+// I—¹ˆ—‚ğ‚·‚é
+void Pad_Finalize( Pad *Pad );
+
+#endif
