16e37d0969873c25967504bf34a3d592912cf59f
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2012-12-30 19:05:27 +0000

    r54

--- Camera.cpp
+++ Camera.cpp
@@ -3,29 +3,63 @@
 #include <stdio.h>
 #include "pad.h"
 
+#define ROTE 1.570796
+
 struct Camera
 {
 	int mode;
 	VECTOR cam;
 	VECTOR pt;
-}; 
+	float HRotate;
+	int swit;
+	int count;
+};
+
+float cam_pos [8][3] =
+{
+	{17,250,-310}, {-182,250,-310}, {-379,250,-310}, {-592,250,-310},
+	{-592,250,255}, {-592,250,820}, {158,250,820}, {928,250,450}
+};
+
+float pt_pos [8][3] =
+{
+	{-920,200,-310}, {-920,200,-310}, {-920,200,-310}, {-592,250,-310},
+	{-592,250,1020}, {-592,250,1020}, {-592,250,1020}, {-592,250,1020}
+};
 
 // 初期化をする
 Camera *Camera_Initialize()
 {
 	Camera *self;
 	self = (Camera *)malloc(sizeof(Camera));
-	//self->cam = VGet(0.0f,500.0f,-500.0f);
 	self->mode= 0;
-	self->cam = VGet(1160,150,-130);
-	self->pt = VGet(-920,190,-120);
+	self->cam = VGet(928,250,450);
+	self->pt = VGet(-592,250,1020);
+	self->HRotate = -1.570796;
+	self->swit = 0;
+	self->count  = 0;
 	return self;
 }
 
+void role_cam(Camera *self)
+{
+	if(self->swit != 0)
+	{
+		int sign;
+		int cut = 15;
+		if(self->swit == 1){sign = 1;}
+		else if(self->swit == 2){sign = -1;}
+
+		if(self->count < cut){self->HRotate += sign * ROTE /cut;self->count++;}
+		else{self->count = 0; self->swit = 0;}
+
+	}
+}
 // 動きを計算する
 void Camera_Update( Camera *self )
 {
 	SetCameraPositionAndTarget_UpVecY(self->cam, self->pt);
+	SetCameraPositionAndAngle( self->cam, 0.0f, self->HRotate, 0.0f ) ;
 
 	if(self->mode % 2 == 0)
 	{
@@ -54,22 +88,17 @@ void Camera_Update( Camera *self )
 	}
 
 	//ルンバルンバルンバ
-	if(Pad_Get( KEY_INPUT_RIGHT ) == -1)
-	{
-		
-	}
-	else if(Pad_Get( KEY_INPUT_LEFT ) == -1)
-	{
+	if(self->swit == 0 && Pad_Get( KEY_INPUT_V ) == -1){self->swit = 1;	}
+	else if(self->swit == 0 && Pad_Get( KEY_INPUT_C ) == -1){self->swit = 2;}
 
-	}
-
-
+	role_cam(self);
 
 
 	if(Pad_Get(KEY_INPUT_Z) == -1 )
 	{
 		printf("\ncam(x,y,z) = (%lf,%lf,%lf)\n",self->cam.x,self->cam.y,self->cam.z);
 		printf("\npt(x,y,z) = (%lf,%lf,%lf)\n",self->pt.x,self->pt.y,self->pt.z);
+		printf("\nhrote = %lf\n",self->HRotate);
 	}
 	
 	if(Pad_Get(KEY_INPUT_X) == -1 )
--- Game.cpp
+++ Game.cpp
@@ -28,6 +28,24 @@ Game *Game_Initialize()
 	return self;
 }
 
+void move_area(Game *self)
+{
+	if(self->hougaku == 0)
+	{
+		if(self->area > -1 && self->area < 4 ){self->area++;}
+		else if(self->area > 5 && self->area < 8 ){self->area--;}
+	}
+	else if(self->hougaku == 1){if(self->area > 2 && self->area < 5 ){self->area++;}}
+	else if(self->hougaku == 2)
+	{
+		if(self->area > 4 && self->area < 7 ){self->area++;}
+		else if(self->area > 1 && self->area < 4 ){self->area--;}
+	}
+	else if(self->hougaku == 3){if(self->area > 3 && self->area < 6 ){self->area--;}}
+
+}
+
+
 
 // “®‚«‚ğŒvZ‚·‚é
 void Game_Update(Game *self)
@@ -43,18 +61,19 @@ void Game_Update(Game *self)
 		else{self->hougaku--;}
 	}
 
+	if(Pad_Get( KEY_INPUT_UP ) == -1)
+	{
+		move_area(self);
+
+	}
+
+
+
 	if(Pad_Get( KEY_INPUT_RETURN ) == -1)
 	{
 		printf("\nmuki = %d\n",self->hougaku);
+		printf("\area = %d\n",self->area);
 	}
-	
-
-	/*
-	if(self->area == 0 && self->hougaku == 0 && 1)
-	{
-
-	}
-	*/
 
 	Script_Update( self->script );
 }
