94e6e173bf9ec74232aaa3e6923552e04228d97c
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2013-02-08 03:25:44 +0000

    r103

--- Script.cpp
+++ Script.cpp
@@ -43,11 +43,15 @@ struct Condition_order
 struct Effect_order
 {
 	//命令効果の構造体
-	//x,y,効果番号(0:画像表示 1:文字表示 2:消去)
+	//管理番号・x・y・効果番号(0:画像表示・1:文字表示)・画像の名前・文字・画像文字の管理タグ
 	int num;
 	int x;
 	int y;
 	int effect_num;
+	std::string draw_name;
+	std::string text;
+	std::string tag;
+
 };
 
 struct Script
@@ -62,26 +66,58 @@ struct Script
 	//命令構造体変数 
 	std::vector<Condition_order> condition_order;
 	std::vector<Effect_order> effect_order;
+	int condition_count;
+	int effect_count;
 	
 };
 
 void pack_words(Script *self, Words &line)
 {
-	for(int i = 0; i < line.size(); i++)
+	if(line.size() > 0)
 	{
-		printf("\n %s \n", line[i].c_str());
+		if(line[0].c_str() == "con")
+		{
+			self->condition_order[self->condition_count].num = std::stoi(line[1]);
+			self->condition_order[self->condition_count].area = std::stoi(line[2]);
+			self->condition_order[self->condition_count].hougaku = std::stoi(line[3]);
+			self->condition_order[self->condition_count].order = line[4].c_str();
+			self->condition_order[self->condition_count].object = line[5].c_str();
+
+			if(line.size() > 6)
+			{
+				self->condition_order[self->condition_count].special = line[6].c_str();
+			}
+
+			self->condition_count++;
+		}
+		else if(line[0].c_str() == "eff")
+		{
+			self->effect_order[self->effect_count].num = std::stoi(line[1]);
+
+			if(line[2].c_str() == "draw")
+			{
+				self->effect_order[self->effect_count].draw_name = line[3].c_str();
+			}
+			else if(line[2].c_str() == "text")
+			{
+				self->effect_order[self->effect_count].text = line[3].c_str();
+			}
+
+			self->effect_order[self->effect_count].x = std::stoi(line[4]);
+			self->effect_order[self->effect_count].y = std::stoi(line[5]);
+			self->effect_order[self->effect_count].tag = line[6].c_str();
+
+			self->effect_count++;
+		}
+
 	}
 
 }
 
 int load_script(Script *self, const char *filename)
 {
-	int pos = 0; //現在の文字位置
-	int now_line = 0; //現在の行位置
 	std::string line; //1行読み込み用
-	std::vector<std::vector<std::string>>  words;
 	std::ifstream file(filename); // ファイルを読み込み
-
 
 	if (file.fail())
 	{
@@ -92,10 +128,8 @@ int load_script(Script *self, const char *filename)
 
 	while (std::getline(file, line))
 	{
-		pack_words(self, split(line)); // 分解してつっこむ
+		pack_words(self, split(line)); // １行をブロックにして構造体に代入
 	}
-
-	
 
 	return 0;
 }
@@ -113,6 +147,9 @@ Script *Script_Initialize(Camera *camera, Console *console , Player *player)
 	self->mess = Mess_Initialize( );
 	self->twod = Twod_Initialize( );
 	self->console = console;
+
+	self->condition_count = 0;
+	self->effect_count = 0;
 
 	printf("\nスクリプト読み込み　開始\n\n");
 	load_script(self, "tex/script.txt");
