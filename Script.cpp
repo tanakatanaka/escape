b46237893ff5dd0474adf313aee66589d0ebbdeb
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2013-02-28 15:04:13 +0000

    r142: luaいれたけどなんかおちる

--- Script.cpp
+++ Script.cpp
@@ -18,10 +18,11 @@
 #include "Pad.h"
 #include "Compiler.h"
 
-typedef std::vector<std::string> Words;
-
-Words split(const std::string &str);
-
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
 
 struct Script
 {
@@ -31,14 +32,7 @@ struct Script
 	Twod *twod;
 	Console *console;
 	Player *player;
-
-	//命令構造体変数 
-	std::vector<When> when;
-	std::vector<Condition> condition;
-	std::vector<Condition> notice;
-	std::vector<Effect> effect;
-
-	std::string last_memo;
+  lua_State *lua;
 };
 
 // 初期化をする
@@ -53,15 +47,18 @@ Script *Script_Initialize(Camera *camera, Console *console , Player *player, Roo
 	self->mess = Mess_Initialize( );
 	self->twod = Twod_Initialize( self->player );
 	self->console = console;
-	Compiler *compiler = Compiler_Initialize( );
 	
-	CompilerResult result = Compiler_compile(compiler, "tex/script.txt");
-
-	self->when = result.when;
-	self->condition = result.condition;
-	self->notice = result.notice;
-	self->effect = result.effect;
-	self->last_memo = "non";
+	fprintf(stderr, "a\n");
+	
+	self->lua = luaL_newstate();
+	luaL_openlibs(self->lua);
+	
+  if (luaL_loadfile(self->lua, "script\\script.lua") == LUA_ERRFILE)
+  {
+		  MessageBox(NULL, "script/script.lua が読み込めませんでした", "ゲームのエラー", 0);
+		  exit(1);
+  }
+  
 	return self;
 }
 
@@ -72,88 +69,6 @@ bool area_match(const Condition &c, Player *player)
 		   c.hougaku == Player_get_hougaku(player);
 }
 
-bool condition_match(const Condition &c, Words &words)
-{
-	return words.size() >= 2 &&
-		   c.order == words[0] &&
-		   c.object == words[1];
-}
-
-void action_match(Script *self, Words &act)
-{
-	Room_act(self->room, act);
-	Player_act(self->player, act);
-	
-}
-
-void call_effect(Script *self, const Condition &c)
-{
-	for(int j = 0; j < (int)self->effect.size(); j++)
-	{
-		if(c.effect_name == self->effect[j].name)
-		{
-			Effect &e = self->effect[j];
-
-			if(e.effect_type == "draw")
-			{
-				if(self->last_memo != c.effect_name) 
-				{ 
-					Twod_erase_image(self->twod);
-					self->last_memo = c.effect_name;
-				}
-				Twod_add_image(self->twod, e.x, e.y, e.draw_id);
-			}
-			else if(e.effect_type == "text")
-			{
-				if(self->last_memo != c.effect_name)
-				{ 
-					Mess_erase_word(self->mess); 
-					self->last_memo = c.effect_name;
-				}
-				Mess_add_word(self->mess, e.x, e.y, e.text.c_str());
-			}
-			else if(e.effect_type == "act")
-			{
-				action_match(self, e.action);
-			}
-		}
-
-	}
-	self->last_memo = "non"; 
-}
-
-void word_act(Script *self, Words &words)
-{	
-	if(words.size() > 0)
-	{
-		for(int i = 0; i < (int)self->condition.size(); i++)
-		{
-			Condition &c = self->condition[i];
-
-			if(area_match(c, self->player) && condition_match(c , words))
-			{
-				call_effect(self, c);
-			}
-		}
-	}
-}
-
-void check_notice(Script *self)
-{	
-	for(int i = 0; i < (int)self->notice.size(); i++)
-	{
-		Condition &n = self->notice[i];
-
-		if(area_match(n, self->player) && n.one_time == true)
-		{
-			call_effect(self, n);
-			n.one_time = false;
- 		}
-	}
-}
-
-
-
 void decode_command(Script *self)
 {
 	//単語後に分解
@@ -161,11 +76,9 @@ void decode_command(Script *self)
 		
 	if(command != NULL)
 	{
-		Words words = split(command);
 		//コンソールのほうにあるコマンドをログに移動
 		console_shift_log(self->console);
 		//分解したwordを解読関数にかける
-		word_act(self,words);
 	}
 }
 
@@ -179,8 +92,6 @@ void Script_Update( Script *self )
 	{
 		decode_command(self);
 	}
-	
-	check_notice(self);
 }	
 
 // 描画する
@@ -194,5 +105,5 @@ void Script_Draw( Script *self)
 // 終了処理をする
 void Script_Finalize( Script *self )
 {
-    
+    lua_close(self->lua);
 }
---
+++ lua_one.c
@@ -0,0 +1,39 @@
+#define LUA_ANSI
+#define LUA_LIB
+
+#define ltable_c
+
+#include "lobject.c"
+#include "lapi.c"
+#include "lcode.c"
+#include "lctype.c"
+#include "ldebug.c"
+#include "ldo.c"
+#include "ldump.c"
+#include "lfunc.c"
+#include "lgc.c"
+#include "llex.c"
+#include "lmem.c"
+#include "lopcodes.c"
+#include "lparser.c"
+#include "lstate.c"
+#include "lstring.c"
+#include "ltable.c"
+#include "ltm.c"
+#include "lundump.c"
+#define pushclosure pushclosure2
+#include "lvm.c"
+#include "lzio.c"
+#include "lauxlib.c"
+
+#include "lbaselib.c"
+#include "lbitlib.c"
+#include "lcorolib.c"
+#include "ldblib.c"
+#include "liolib.c"
+#include "lmathlib.c"
+#include "loadlib.c"
+#include "loslib.c"
+#include "lstrlib.c"
+#include "ltablib.c"
+#include "linit.c"
