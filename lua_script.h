5a140596255cfc1a52279910315abfd023333513
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2013-03-03 13:36:04 +0000

    r159: lua_script: 型指定

--- lua_script.h
+++ lua_script.h
@@ -10,8 +10,13 @@ LuaScript *LuaScript_Initialize();
 /** ファイルを読み込む。エラーだったらfalseを返す。 */
 bool LuaScript_Load(LuaScript *self, const char *filename);
 
-/** スクリプト内で使う変数を設定する。 */
-void LuaScript_Set(LuaScript *self, const char *name, void *data);
+/**
+  スクリプト内で使う変数(なんかの構造体のポインタに限る)を設定する。
+  type: 型名
+  name: 変数名
+  data: ポインタ
+*/
+void LuaScript_Set(LuaScript *self, const char *type, const char *name, void *data);
 
 /** ファイルを閉じる。 */
 void LuaScript_Finalize(LuaScript *);
