bda5e3200136ea903b7441656ba5e57a1840b2e6
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2013-03-17 16:37:33 +0000

    r228: make_lua_headerをビルドターゲットに組み込んだ

--- make_lua_header.bat
+++ make_lua_header.bat
@@ -1,12 +1,1 @@
-@echo off
-for /f %%r in ('dir /b /OD lua_header.h lua_header.pkg') do (
-	if %%r equ lua_header.h (
-		echo running tolua..
-		tolua -o lua_header.h lua_header.pkg
-	) else if %%r equ lua_header.pkg (
-		echo lua_header.h already updated
-	) else (
-		echo "unexpected"
-	)
-	exit /b %errorlevel%
-)
+@tolua -o lua_header.h lua_header.pkg
